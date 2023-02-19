85457512f92f0c2d204ff7eb63a1c6d7b4ec1447
Nicholas Peters <peters.nicholas@gmail.com>
2023-02-19 17:02:10 +0100

    add current and three hour weather, dynamic bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,46 @@
-import Card from './components/Card'
+import { useEffect, useState } from 'react'
+import { IWeatherData } from './services/IWeatherData'
+import { WeatherDataService } from './services/WeatherDataService'
+import CurrentWeather from './components/CurrentWeather'
 import Sidebar from './components/Sidebar'
 import WeatherDashboard from './components/WeatherDashboard'
 import Sunnyday from './assets/sunny-day.png'
 import Rainyday from './assets/rainy-day.png'
+import Cloudyday from './assets/cloudy-day.png'
 
 function App() {
+  const [weatherData, setWeatherData] = useState<IWeatherData | null>(null);
 
-  let isRainy = true
+  useEffect(() => {
+      WeatherDataService.getWeatherData(57.7, 11.96)
+      .then(response => setWeatherData(response));      
+  }, []);    
+
+  if (!weatherData) {
+      return <div>Loading...</div>;
+  }
+
+  let backgroundImage;
+  const weatherMain = weatherData?.list[0].weather[0].main;
+  
+  switch (weatherMain) {
+    case 'Rain':
+      backgroundImage = Rainyday;
+      break;
+    case 'Clear':
+      backgroundImage = Sunnyday;
+      break;
+    case 'Clouds':
+      backgroundImage = Cloudyday;
+      break;
+    default:
+      backgroundImage = Sunnyday;
+  }
 
   return (
-    <div className="App flex bg-cover bg-center bg-fixed" style={{ backgroundImage: `url(${isRainy ? Rainyday : Sunnyday})` }}>
+    <div className="App min-h-screen flex flex-col bg-cover bg-center bg-fixed p-4" 
+      style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <CurrentWeather />
       <Sidebar />
       <WeatherDashboard />
     </div>
