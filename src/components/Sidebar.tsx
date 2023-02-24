05dac111d72e303906bc1b408e471d306734b4c0
Nicholas Peters <peters.nicholas@gmail.com>
2023-02-24 18:19:31 +0100

    weather added to state

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,18 +1,32 @@
+import { ChangeEvent, useState } from "react";
+import { getWeatherData } from "../services/WeatherDataService";
+
 export default function Sidebar() {
+    // skapa state för väderdata, skicka väderdata till komponent med props
+
+    const [city, setCity] = useState("");
+    const [weather, setWeather] = useState("");
     
-    function getSearchInput(event: React.FormEvent<HTMLFormElement>) {
+    async function getSearchInput(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
-        const searchInputValue = (document.getElementById('searchInput') as HTMLInputElement)?.value;
-        return searchInputValue;
+
+        let response = await getWeatherData(city);
+        setWeather(JSON.stringify(response));
+        console.log(response);
+    }
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setCity(e.target.value);
     }
 
     return (
         <div className="p-8 w-fit mx-auto glass border border-slate-900 rounded-xl">
             <h1 className="my-4 text-center text-2xl">SkyView</h1>
             <form id="searchForm" onSubmit={getSearchInput} className="flex flex-col gap-8">
-                <input id="searchInput" type="text" placeholder="Search" className="w-full p-2 rounded-lg" />
+                <input id="searchInput" type="text" placeholder="Search" className="w-full p-2 rounded-lg" value={city} onChange={handleChange}/>
                 <button type="submit" className="p-2 mx-auto rounded-lg bg-white">Search</button>
             </form>
+            <p>{weather}</p>
         </div>
     )
 }
--- src/services/GeocodingDataService.ts
+++
@@ -1,4 +0,0 @@
-import axios from "axios";
-
-const API_Key = "b16d3c460c63b50c63635b2ac02b0f46"
-
